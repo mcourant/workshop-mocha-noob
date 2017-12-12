0c9c5b27b506cdc5d1d51e5336ffaedf80db93ca
Maxime COURANT <maxime.courant@ynov.com>
2017-12-12 14:48:27 +0100

    Exo 2

--- test/shop-tools.test.js
+++ test/shop-tools.test.js
@@ -1,8 +1,13 @@
 var expect = require("chai").expect
 var shop = require("../src/shop-tools")
-console.log(shop.getPrices())
+
+
 describe('Hello world testing !', function() {
     it('good prices', function() {
         expect(shop.getPrices()).to.eql({ banana: 1, potato: 2, tomato: 3, cucumber: 4, salad: 5, apple: 6 });
+    })
+    const products = [ 'tomato', 'cucumber', 'tomato', 'salad', 'potato', 'cucumber', 'potato', 'potato', 'tomato', 'potato' ];
+    it('count articles', function() {
+        expect(shop.countArticle(products)).to.eql(10);
     })
 });
